940f102f8866d3f6328f4a8b635a0c3f3718d036
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2024-02-22 17:19:28 -0500

    pre-compile Stan code

---
+++ data-raw/stan/stan-scripts.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+devtools::load_all()
+
+qmatrix <- tibble::tibble(att_1 = c(1, 0, 1, 0, 1, 1),
+                          att_2 = c(0, 1, 0, 1, 1, 1))
+
+create_stan_tdcm(q_matrix = qmatrix) %>%
+  write_lines("inst/stan/tdcm.stan")
+
+create_threaded_stan_tdcm(q_matrix = qmatrix) %>%
+  write_lines("inst/stan/threaded-tdcm.stan")
+
+create_fng_stan_tdcm(q_matrix = qmatrix) %>%
+  write_lines("inst/stan/fng-tdcm.stan")
+
+create_fng_no_common_items_stan_tdcm(q_matrix = qmatrix) %>%
+  write_lines("inst/stan/fng-no-common-tdcm.stan")
