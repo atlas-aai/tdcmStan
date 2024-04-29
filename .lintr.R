ac8c164bac6a84a3bc820eb4d6dd77497d4d81dd
JeffreyCHoover <jeffrey.c.hoover@gmail.com>
2024-04-29 11:12:45 -0400

    update lintr checks

--- .lintr.R
+++ .lintr.R
@@ -1,8 +1,11 @@
-linters: with_defaults(
-  dummy_linter = NULL
-)
+linters: with_defaults()
 exclusions: list(
-  "man/create_fng_stan_tdcm.Rd",
-  "man/create_stan_tdcm.Rd",
-  "man/create_threaded_stan_tdcm.Rd"
-)
+    "R/create-fng-tdcm-no-common-item.R",
+    "R/create-fng-tdcm.R",
+    "R/create-tdcm",
+    "R/create-threaded-tdcm.R",
+    "man/create_fng_stan_tdcm.Rd",
+    "man/create_stan_tdcm.Rd",
+    "man/create_threaded_stan_tdcm.Rd"
+  )
+encoding: "UTF-8"
